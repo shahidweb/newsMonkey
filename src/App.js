d075d24165d147941785804d550ddf54883eb026
Shahid Khan <shahidsaifi88@gmail.com>
2023-04-15 16:18:19 +0530

    classbesedNewsMonkey

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import NavBar from './components/NavBar'
+import News from './components/News'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  pageSize = 8;
+  state = { progress: 0 }
+  APIKEY = process.env.REACT_APP_NEWS_APIKEY;
+  setPrograss = (progress) => { this.setState({ progress: progress }) }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <NavBar />
+          {/* <News pageSize={5} country='in' category="science" /> */}
+          <LoadingBar
+            color='#f11946'
+            height={3}
+            progress={this.state.progress}
+          />
+          <Routes>
+            <Route path='/' element={<News setPrograss={this.setPrograss} apiKey={this.APIKEY} key="general" pageSize={this.pageSize} country='in' category="general" />} />
+            <Route path='/business' element={<News setPrograss={this.setPrograss} apiKey={this.APIKEY} key="business" pageSize={this.pageSize} country='in' category="business" />} />
+            <Route path='/entertainment' element={<News setPrograss={this.setPrograss} apiKey={this.APIKEY} key="entertainment" pageSize={this.pageSize} country='in' category="entertainment" />} />
+            <Route path='/general' element={<News setPrograss={this.setPrograss} apiKey={this.APIKEY} key="general" pageSize={this.pageSize} country='in' category="general" />} />
+            <Route path='/health' element={<News setPrograss={this.setPrograss} apiKey={this.APIKEY} key="health" pageSize={this.pageSize} country='in' category="health" />} />
+            <Route path='/science' element={<News setPrograss={this.setPrograss} apiKey={this.APIKEY} key="science" pageSize={this.pageSize} country='in' category="science" />} />
+            <Route path='/sports' element={<News setPrograss={this.setPrograss} apiKey={this.APIKEY} key="sports" pageSize={this.pageSize} country='in' category="sports" />} />
+            <Route path='/technology' element={<News setPrograss={this.setPrograss} apiKey={this.APIKEY} key="technology" pageSize={this.pageSize} country='in' category="technology" />} />
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+  render() {
+    let { title, description, imgurl, newsUrls, author, source, date } = this.props;
+    return (
+      <div className='my-3'>
+        <div className="card">
+          <img src={imgurl ? imgurl : 'https://images.moneycontrol.com/static-mcnews/2023/04/stocks_sensex_nifty_stockmarket-2-770x433.jpg'} className="card-img-top" alt="..." />
+          <span className="position-absolute badge rounded-pill end-0 bg-danger">{source}</span>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-body-secondary">By {author} on {new Date(date).toDateString()}</small></p>
+            <a rel="noopener noreferrer" href={newsUrls} className="btn btn-sm btn-primary" target='_blank'>Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
